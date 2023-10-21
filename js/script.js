c317f4b38aab95a56d0d3be3686ed753b4e0a165
CheAbdullah <chezulkhairi@gmail.com>
2023-10-21 10:40:21 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,12 +12,12 @@ L.tileLayer(map1URL).addTo(before);
 L.tileLayer(map2URL).addTo(after);
 
 // Draw a red circle on 'Before Map'
-const circle1 = L.circle(center, {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.2,
-    radius: 5000
-}).addTo(before);
+//const circle1 = L.circle(center, {
+//    color: 'red',
+//    fillColor: '#f03',
+//    fillOpacity: 0.2,
+//    radius: 5000
+//}).addTo(before);
 
 // Draw a blue circle on 'After Map'
 const circle2 = L.circle(center, {
